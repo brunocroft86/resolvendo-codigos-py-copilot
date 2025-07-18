6426122d1b5a9d46b7b11848246c3acc24dcce27
Bruno Galvão <97357628+brunocroft86@users.noreply.github.com>
2025-07-18 22:28:38 +0000

    meu comite desafio.

---
+++ concat_dados.py
@@ -0,0 +1,6 @@
+info1 = input("Digite a primeira informação:")
+info2 = input("Digite a segunda informação:")
+
+info_concatenada = info1 + " " + info2
+
+print("A informação concatenada é:", info_concatenada)
